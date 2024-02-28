e8c0b9472b5f6407205fb6ea83cb148ac2ae934f
Nolene Human <108449986+Nolene-Human@users.noreply.github.com>
2024-02-28 06:31:56 +0000

    updated with daily prediction for home town

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
-credit to 
-NeuralNine (https://www.youtube.com/watch?v=9P5MY_2i7K8&t=47s)
+This Application will call the daily weather from AccuWeather API for Amberley
+
+credit to NeuralNine (https://www.youtube.com/watch?v=9P5MY_2i7K8&t=47s) for calling API
 
 '''
 
@@ -13,12 +14,51 @@ def convert_to_celsius(fahrenheit):
     return celsius
     
 
-base_url="http://dataservice.accuweather.com/forecasts/v1/daily/1day/247035?" #this is just for Amberley
+base_url="http://dataservice.accuweather.com/forecasts/v1/daily/1day/" #this is just for Amberley
+
+location_key="247035"
 
 api_key="vj4GmLNRQGrvg3p8CffKuYoYRFgAnlJe"
 
-url=base_url + "apikey=" +api_key
+url=base_url + location_key+ "?apikey=" +api_key
 
 response=requests.get(url).json()
 
-print(response)
+#print(response)
+'''
+daily_forecasts = response.get('DailyForecasts')
+if daily_forecasts:
+    for forecast in daily_forecasts:
+        date = forecast.get('Date')
+        temperature = forecast.get('Temperature')
+        day = forecast.get('Day')
+        night = forecast.get('Night')
+
+        print(f"Date: {date}")
+        print(f"Temperature: {temperature}")
+        print(f"Day: {day}")
+        print(f"Night: {night}")
+        print("\n")
+else:
+    print("No daily forecasts available")
+'''
+
+#This code will get the temperature
+daily_forecasts = response.get('DailyForecasts')
+if daily_forecasts:
+    for forecast in daily_forecasts:
+        day=forecast.get('Day',{}).get('PrecipitationIntensity') +" " + forecast.get('Day',{}).get('PrecipitationType')
+        date = forecast.get('Date')
+        date = date.split('T')[0]  # Split the date at 'T' and take the first part
+        temperature1 = forecast.get('Temperature', {}).get('Minimum').get('Value')
+        minimum = int(convert_to_celsius(temperature1))
+        temperature2=forecast.get('Temperature', {}).get('Maximum').get('Value')
+        maximum = int(convert_to_celsius(temperature2))
+        
+        print(f'Your day will have ' + day)
+        print(f"Date: {date}")
+        print(f"Maximum Temperature: {maximum} / Minimum Temperature: {minimum}")
+        
+        print("\n")
+else:
+    print("No daily forecasts available")
